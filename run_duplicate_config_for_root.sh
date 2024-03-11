ce068b4627987ab5676e1ed550a6920d9fdca1d1
Samuel Maier <samuel.maier2@hotmail.de>
2024-03-11 13:34:49 +0100

    add documentation

--- run_duplicate_config_for_root.sh
+++ run_duplicate_config_for_root.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+# Copy selected config files to the root user
 
 set -e
 
+# could not find a better way to get to the chezmoi directory
 cd ~/.local/share/chezmoi
 
+# in here we copy all files
 sudo sh <<EOF
     cp -r ./dot_config/helix /root/.config
 EOF
