9f177d1b58e89e28e652463675c5af3149a0b8d0
Samuel Maier <samuel.maier2@hotmail.de>
2024-03-11 14:34:03 +0100

    switch to linking for config files

---
+++ run_onchange_link_config_for_root.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Link selected config files to the root user
+# they better not be security relevant!
+# but that way we dont have to execute on every chezmoi update
+# or do complicated checks for differences, 
+# and adjustments to user get immediately applied 
+# without the need for a chezmoi apply between.
+# 
+# note, I've only tested this to work with helix.
+# I've seen some software refuse symbolically linked config files.
+
+set -e
+
+# could not find a better way to get to the chezmoi directory
+cd ~/.local/share/chezmoi
+
+# in here we link all files
+sudo sh <<EOF
+    ln -sf "$PWD"/dot_config/helix /root/.config/
+EOF
