0cda2193c737f6be038eb0533774c8d7eb86ef61
Samuel Maier <samuel.maier2@hotmail.de>
2024-05-22 21:19:40 +0000

    Update dot_config/posix_shells/init.sh

--- dot_config/posix_shells/init.sh
+++ dot_config/posix_shells/init.sh
@@ -1,5 +1,4 @@
-source ~/.config/posix_shells/plugins.sh
-source ~/.config/posix_shells/alias.sh
-source ~/.config/posix_shells/export_env.sh
 source ~/.config/posix_shells/path.sh
-
+source ~/.config/posix_shells/alias.sh
+source ~/.config/posix_shells/plugins.sh
+source ~/.config/posix_shells/export_env.sh
